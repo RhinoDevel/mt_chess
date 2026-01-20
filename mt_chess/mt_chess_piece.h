47250ecf05a110ddd67a456b160accabc1412032
Marc <RhinoDevel@users.noreply.github.com>
2026-01-20 08:36:47 +0100

    CHANGE: Use 255 instead of 0 for empty square / invalid ID (to be able to use index as ID and vice versa).

--- mt_chess/mt_chess_piece.h
+++ mt_chess/mt_chess_piece.h
@@ -18,11 +18,11 @@ extern "C" {
 #endif //__cplusplus
 
 // Signalizes empty square on board AND invalid piece:
-#define MT_CHESS_PIECE_EMPTY 0
+#define MT_CHESS_PIECE_EMPTY UINT8_MAX // To be able to use index as ID.
 
 // See mt_chess_piece_init():
-#define MT_CHESS_PIECE_GET_ID_BY_INDEX(i) ((uint8_t)((i) + 1))
-#define MT_CHESS_PIECE_GET_INDEX_BY_ID(id) ((int)(id) - 1)
+#define MT_CHESS_PIECE_GET_ID_BY_INDEX(i) ((uint8_t)(i))
+#define MT_CHESS_PIECE_GET_INDEX_BY_ID(id) ((int)(id))
 
 struct mt_chess_piece // Not just an enum., e.g., because of pawn promotion.
 {
